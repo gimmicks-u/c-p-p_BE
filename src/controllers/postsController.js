fdcb03046dfc3c12eef41a272c94c9fb5f157948
gimmicks-u <is@pusan.ac.kr>
2022-07-18 11:47:18 +0900

    [refactor] post 등록 로직변경
    controller, service 로직 변경

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -10,8 +10,8 @@ exports.selectPosts = (req, res, next) => {
 };
 
 exports.createPost = async (req, res) => {
+  // dev : 현재는 userId를 body에서 가져오게 설정.
   // const { userId } = req.user.id;
-
   const {
     userId,
     content,
@@ -23,38 +23,17 @@ exports.createPost = async (req, res) => {
     rate,
   } = req.body;
 
-  let postId = null;
-  try {
-    // 1. post
-    const postDTO = {
-      userId,
-      cafeId,
-      content,
-      visited,
-      receiptURL,
-      isSponsored,
-    };
-    postId = await postsService.createPost(postDTO);
+  const postDTO = {
+    userId,
+    cafeId,
+    content,
+    visited,
+    receiptURL,
+    isSponsored,
+  };
 
-    // 2. photo
-    // ex) photoDTO = [[postId, '1번 URL'], [postId, '2번 URL']...]
-    const photoDTO = photoURLs.map((url) => [postId, url]);
-    await postsService.createPhoto(photoDTO);
+  const result = await postsService.createPost(postDTO, rate, photoURLs);
+  const { status, ...response } = result;
 
-    // 3. rate
-    const rateDTO = { postId, ...rate };
-    await postsService.createRate(rateDTO);
-  } catch (err) {
-    console.log(err);
-    res.status(400);
-    res.json({
-      message: '입력 정보를 확인해주세요',
-    });
-  }
-
-  res.status(201);
-  res.json({
-    id: postId,
-    message: '게시물이 등록되었습니다.',
-  });
+  res.status(status).json(response);
 };
--- src/services/postsService.js
+++ src/services/postsService.js
@@ -11,7 +11,7 @@ exports.selectPosts = (userId, cafeId) => {
 
 // exports.selectPostsWithCafesByStoredUser = (queryString) => {};
 
-exports.createPost = async (postDTO) => {
+exports.createPost_ = async (postDTO) => {
   const postId = await postsDao.createPost(postDTO);
   return postId;
 };
@@ -24,4 +24,25 @@ exports.createPhoto = async (photoDTO) => {
 exports.createRate = async (rateDTO) => {
   const rateId = await postsDao.createRate(rateDTO);
   return rateId;
+};
+
+exports.createPost = async (postDTO, rate, photoURLs) => {
+  try {
+    // 1. db-posts 부분 등록
+    const postId = await postsDao.createPost(postDTO);
+
+    // 2. db-photos 부분 등록
+    // ex) photoDTO = [[postId, '1번 URL'], [postId, '2번 URL']...]
+    const photoDTO = photoURLs.map((url) => [postId, url]);
+    const photosInsertId = await postsDao.createPhoto(photoDTO);
+
+    // 3. db-rate 부분 등록
+    const rateDTO = { postId, ...rate };
+    const rateInsertId = await postsDao.createRate(rateDTO);
+
+    return { id: postId, message: '게시물이 등록되었습니다.', status: 200 };
+  } catch (err) {
+    console.log(err);
+    return { message: '잘못된 요청입니다', status: 400 };
+  }
 };

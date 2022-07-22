339b668048c45e2794fdcee6f6ae1107de7ea797
gimmicks-u <is@pusan.ac.kr>
2022-07-22 11:00:38 +0900

    [refactor] post인가 리팩토링
    try-catch문 추가
    posts 인가, users 인가 유효성검사 추가

--- src/middlewares/custom/authorization/postsAuthorizator.js
+++ src/middlewares/custom/authorization/postsAuthorizator.js
@@ -2,12 +2,21 @@ const postsDao = require('../../../daos/postsDao');
 
 exports.isAuthorizedUserPost = async (req, res, next) => {
   const loginUserId = req.user.id;
-  const requestedPostId = req.params.id; // postId
+  let requestedPostId = req.params.id; // postId
 
-  const result = await postsDao.selectUserIdByPostId(requestedPostId);
-  console.log(typeof result);
+  isNaN(requestedPostId)
+    ? res.status(400).json({ message: '입력하신 정보를 확인해주세요.' })
+    : (requestedPostId = Number(requestedPostId));
 
-  loginUserId !== result
-    ? res.status(403).json({ message: '요청을 처리할 권한이 없습니다.' })
-    : next();
+  try {
+    const result = await postsDao.selectUserIdByPostId(requestedPostId);
+    console.log(typeof result);
+
+    loginUserId !== result
+      ? res.status(403).json({ message: '요청을 처리할 권한이 없습니다.' })
+      : next();
+  } catch (err) {
+    console.log(err);
+    res.status(403).json({ message: '잘못된 요청입니다.' });
+  }
 };
--- src/middlewares/custom/authorization/usersAuthorizator.js
+++ src/middlewares/custom/authorization/usersAuthorizator.js
@@ -1,6 +1,10 @@
 exports.isAuthorizedUser = (req, res, next) => {
   const loginUserId = req.user.id;
-  const requestedUserId = req.param.id;
+  let requestedUserId = req.param.id;
+
+  isNaN(requestedUserId)
+    ? res.status(400).json({ message: '입력하신 정보를 확인해주세요.' })
+    : (requestedUserId = Number(requestedUserId));
 
   loginUserId !== requestedUserId
     ? res.status(403).json({ message: '요청을 처리할 권한이 없습니다.' })
